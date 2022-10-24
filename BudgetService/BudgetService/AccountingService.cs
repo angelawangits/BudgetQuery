32c5b24233cecba9eb5fb5a15a66602c632184e9
angela~ <53804450+angelawangits@users.noreply.github.com>
2022-10-24 18:25:00 +0800

    done

--- BudgetService/BudgetService/AccountingService.cs
+++ BudgetService/BudgetService/AccountingService.cs
@@ -11,6 +11,57 @@ public class AccountingService
 
     public decimal Query(DateTime start, DateTime end)
     {
-        return 1000;
+        if (start > end)
+        {
+            return 0;
+        }
+        
+        var budgets = _budgetRepo.GetAll();
+
+        if (IsSameYearMonth(start, end))
+        {
+            var budget = GetBudget(start,budgets);
+            var queryDays = (end - start).Days + 1;
+            return queryDays * (budget.Amount / DateTime.DaysInMonth(start.Year, start.Month));
+        }
+
+        var startBudget = GetBudget(start, budgets);
+        var amountOfStart = (DateTime.DaysInMonth(start.Year, start.Month) - start.Day + 1)*GetDaysAmount(start, startBudget.Amount);
+        
+        var endDateBudget = GetBudget(end, budgets);
+        var amountOfEnd = end.Day*(GetDaysAmount(end, endDateBudget.Amount));
+
+        var current = start.AddMonths(1);
+        var totalMiddleAmount = 0;
+        while (current < new DateTime(end.Year,end.Month,1))
+        {
+            var currentBudget = budgets.FirstOrDefault(x => x.YearMonth == current.ToString(
+                "yyyyMM"));
+            totalMiddleAmount += currentBudget.Amount;
+            current = current.AddMonths(1);
+        } 
+            
+        return amountOfStart + totalMiddleAmount +amountOfEnd;
+    }
+
+    private static decimal GetDaysAmount(DateTime dateTime, int amount)
+    {
+        return amount/DateTime.DaysInMonth(dateTime.Year, dateTime.Month);
+    }
+
+    private static Budget? GetBudget(DateTime dateTime, List<Budget> budgets)
+    {
+        var budget = budgets.FirstOrDefault(x => x.YearMonth == dateTime.ToString("yyyyMM"));
+        
+        if (budget == null)
+        {
+            return new Budget();
+        }
+        return budget;
+    }
+
+    private static bool IsSameYearMonth(DateTime start, DateTime end)
+    {
+        return start.ToString("yyyyMM") == end.ToString("yyyyMM");
     }
 }
--- BudgetService/BudgetService/AccountingServiceTests.cs
+++ BudgetService/BudgetService/AccountingServiceTests.cs
@@ -33,6 +33,168 @@ public class AccountingServiceTests
         totalAmount.Should().Be(1000);
     }
 
+    [Test]
+    public void two_days_in_same_month_with_31k()
+    {
+        GivenAllBudgets(new List<Budget>()
+        {
+            new Budget()
+            {
+                YearMonth = "202210",
+                Amount = 31000
+            }
+        });
+
+        var totalAmount = WhenQuery(new DateTime(2022, 10, 15), new DateTime(2022, 10, 16));
+
+        totalAmount.Should().Be(2000);
+    }
+
+    [Test]
+    public void full_month_with_31k()
+    {
+        GivenAllBudgets(new List<Budget>()
+        {
+            new Budget()
+            {
+                YearMonth = "202210",
+                Amount = 31000
+            }
+        });
+
+        var totalAmount = WhenQuery(new DateTime(2022, 10, 1), new DateTime(2022, 10, 31));
+
+        totalAmount.Should().Be(31000);
+    }
+
+    [Test]
+    public void cross_month_with_31k_in_oct_3k_in_nov()
+    {
+        GivenAllBudgets(new List<Budget>()
+        {
+            new Budget()
+            {
+                YearMonth = "202210",
+                Amount = 31000
+            },
+            new Budget()
+            {
+                YearMonth = "202211",
+                Amount = 3000
+            },
+        });
+
+        var totalAmount = WhenQuery(new DateTime(2022, 10, 31), new DateTime(2022, 11, 2));
+
+        totalAmount.Should().Be(1200);
+    }
+
+    [Test]
+    public void cross_month_with_31k_in_oct_3k_in_nov_31k_in_dec()
+    {
+        GivenAllBudgets(new List<Budget>()
+        {
+            new Budget()
+            {
+                YearMonth = "202210",
+                Amount = 31000
+            },
+            new Budget()
+            {
+                YearMonth = "202211",
+                Amount = 3000
+            },
+            new Budget()
+            {
+                YearMonth = "202212",
+                Amount = 31000
+            },
+        });
+
+        var totalAmount = WhenQuery(new DateTime(2022, 10, 30), new DateTime(2022, 12, 5));
+
+        totalAmount.Should().Be(2000 + 3000 + 5000);
+    }
+
+    [Test]
+    public void cross_month_with_31k_in_oct_3k_in_nov_31k_in_dec_during_oct_5_dec_10()
+    {
+        GivenAllBudgets(new List<Budget>()
+        {
+            new Budget()
+            {
+                YearMonth = "202210",
+                Amount = 31000
+            },
+            new Budget()
+            {
+                YearMonth = "202211",
+                Amount = 3000
+            },
+            new Budget()
+            {
+                YearMonth = "202212",
+                Amount = 31000
+            },
+        });
+
+        var totalAmount = WhenQuery(new DateTime(2022, 10, 5), new DateTime(2022, 12, 10));
+
+        totalAmount.Should().Be(27000 + 3000 + 10000);
+    }
+
+    [Test]
+    public void cross_month_with_3k_in_sep_31k_in_oct_3k_in_nov_31k_in_dec_during_sep_30_dec_2()
+    {
+        GivenAllBudgets(new List<Budget>()
+        {
+            new Budget
+            {
+                YearMonth = "202209",
+                Amount = 3000
+            },
+            new Budget()
+            {
+                YearMonth = "202210",
+                Amount = 31000
+            },
+            new Budget()
+            {
+                YearMonth = "202211",
+                Amount = 3000
+            },
+            new Budget()
+            {
+                YearMonth = "202212",
+                Amount = 31000
+            },
+        });
+
+        var totalAmount = WhenQuery(new DateTime(2022, 9, 30), new DateTime(2022, 12, 2));
+
+        totalAmount.Should().Be(100 + 31000 + 3000 + 2000);
+    }
+
+    [Test]
+    public void invalid_start_end_date()
+    {
+        GivenAllBudgets(new List<Budget>());
+
+        var totalAmount = WhenQuery(new DateTime(2022, 10, 16), new DateTime(2022, 10, 15));
+
+        totalAmount.Should().Be(0);
+    }
+
+    [Test]
+    public void when_no_budget()
+    {
+        GivenAllBudgets(new List<Budget>());
+
+        var totalAmount = WhenQuery(new DateTime(2022, 10, 16), new DateTime(2022, 10, 16));
+
+        totalAmount.Should().Be(0); 
+    }
+    
     private decimal WhenQuery(DateTime startDate, DateTime endDate)
     {
         var totalAmount = _accountingService.Query(startDate, endDate);
